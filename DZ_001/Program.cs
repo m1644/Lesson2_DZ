e80ca2524d6e1c4131efd811406b50218147f8c8
MaxX <maxx-f@mail.ru>
2022-08-09 14:19:08 +0300

    Add DZ_001

---
+++ DZ_001/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int new_number = (number % 100) / 10;
+System.Console.WriteLine(new_number);
+
+// Строчное решение!
+//Console.WriteLine("Введите трёхзначное число: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//string str_number = number.ToString();
+//char digit1 = str_number[0];
+//char digit2 = str_number[1];
+//char digit3 = str_number[2];
+//string new_number = digit2.ToString();
+//System.Console.WriteLine(new_number);
