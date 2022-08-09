29f54f9875993d626ddbcdca5ac8d51e5058228d
MaxX <maxx-f@mail.ru>
2022-08-09 14:52:07 +0300

    Add DZ_002

---
+++ DZ_002/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string new_number = Convert.ToString(number);
+if (new_number.Length > 2)
+{
+  Console.WriteLine("Третья цифра: " + new_number[2]);
+}
+else 
+{
+  Console.WriteLine("Третьей цифры нет!");
+}
