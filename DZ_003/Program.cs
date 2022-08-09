836680f9f0443fc98b34a96f1a5485c7b0d925b3
MaxX <maxx-f@mail.ru>
2022-08-09 15:39:09 +0300

    Add DZ_003

---
+++ DZ_003/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+// ВОПРОС!!! (Почему код работает с фигурными скобками и без них?)
+
+// Без скобок
+//Console.Write("Введи цифру, обозначающую день недели: ");
+//int day_number = Convert.ToInt32(Console.ReadLine());
+//if (day_number < 0 || day_number > 7) Console.WriteLine("Это не день недели!");
+//else if (day_number == 6 || day_number == 7) Console.WriteLine("Этот день - выходной");
+//else Console.WriteLine("Этот день - не выходной");
+
+// С фигурными скобками
+Console.Write("Введи цифру, обозначающую день недели: ");
+int day_number = Convert.ToInt32(Console.ReadLine());
+if (day_number < 0 || day_number > 7) 
+{
+    Console.WriteLine("Это не день недели!");
+}
+else if (day_number == 6 || day_number == 7) 
+{
+    Console.WriteLine("Этот день - выходной :)");
+}
+else 
+{
+    Console.WriteLine("Этот день - не выходной :(");
+}
